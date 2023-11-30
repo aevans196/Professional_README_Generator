835e091f8cccc5db501119d1a2daf5daa189d4dc
AAE <alysae2017@gmail.com>
2023-11-30 00:19:33 -0500

    added final chnages

---
+++ Develop/index.js
@@ -0,0 +1,94 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is your project title?',
+
+    'Describe your project.',
+
+    'What are the installation requirements for your project?',
+    
+    'What is your project used for?',
+
+    'Which license does your project have?',
+
+    'Any credits to be given?',
+
+    'How can your project be tested?',
+
+    'Add your Github URL.',
+
+    'Add your Github username.',
+
+    'What is your email address?'
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+    err ? console.error(err) : console.log('Success!'))
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0]
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: questions[1]
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[2]
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: questions[3]
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: questions[4]
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: questions[5]
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: questions[6]
+        },
+        {
+            type: 'input',
+            name: 'giturl',
+            message: questions[7]
+        },
+        {
+            type: 'input',
+            name: 'gituser',
+            message: questions[8]
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions[9]
+        },
+    ]).then((data) => writeToFile('README.md', generateMarkdown(data)));
+    
+}
+
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,71 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if(license) {
+      return '[![License](https://img.shields.io/badge/'+license+'-lightblue.svg)]';
+  } else if(!license) {
+      return '';
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if(license) {
+      return '[(https://choosealicense.com/licenses/'+license+')]';
+  } else if(!license) {
+      return '';
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if(license) {
+      return '## License';
+  } else if(!license) {
+      return '';
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+return `# ${data.title}
+
+## Description
+${data.desc}
+
+## Table of Contents
+[Installation](#installation)
+[Credits](#credits)
+[Usage](#usage)
+[License](#license)
+[Contact info](#contact-info)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+${renderLicenseSection(data.license)}
+${data.license}
+${renderLicenseLink(data.license)}
+
+## Badges
+${renderLicenseBadge(data.license)}
+
+## Contact-info
+${data.giturl}
+${data.gituser}
+${data.email}
+
+## Tests
+${data.test}
+`
+}
+
+module.exports = generateMarkdown;
